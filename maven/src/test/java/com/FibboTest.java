17674409805160c028ebe79f4647ce907af2aa42
YogitaBhagatni <123232581+YogitaBhagatni@users.noreply.github.com>
2023-02-10 18:11:46 +0530

    Add files via upload

---
+++ maven/src/test/java/com/FibboTest.java
@@ -0,0 +1,55 @@
+package com;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import com.first.Fibbo;
+
+public class FibboTest {
+   
+    
+    @Test
+    //write a test case for initial condition
+    public void testFibboForZeroLimit() {
+        
+        assertEquals(Arrays.asList(0), getLimit(0));//check the condition
+        
+    }
+    @Test
+
+    public void testFibboForOneLimit() {
+        
+        assertEquals(Arrays.asList(0,1), getLimit(1));
+        
+    }
+    @Test
+
+    public void testFibboForTwoLimit() {
+        
+        assertEquals(Arrays.asList(0,1,1), getLimit(2));
+        
+    }
+    @Test
+
+    public void testFibboForThreeLimit() {
+        
+        assertEquals(Arrays.asList(0,1,1,2), getLimit(3));
+        
+    }
+    @Test
+
+    public void testFibboForFourLimit() {
+         assertEquals(Arrays.asList(0,1,1,2,3), getLimit(5));
+        
+    }
+    public ArrayList<Integer> getLimit(int limit) {
+        Fibbo fibbo = new Fibbo();//create a fibbo object in Fibbo class
+        return fibbo.perform(limit);//calling the function and return the ArrayList<Integer>
+    }
+    
+
+}
